2ff78c4b9654f14fec19b94853384e6d7f3c259c
gianmillare <gianmillaregsm@gmail.com>
2020-03-08 18:15:27 -0700

    Created more functions inside common.js that will change the promo message after 4 seconds

--- scripts/common.js
+++ scripts/common.js
@@ -1,11 +1,28 @@
 function Common() {
     let self = this;
     // Properties
-    this.promoBar = {
+    this.promoBar = 
+    {
         promoItems: null;
         currentItem: 0;
         numberOfItems: 0,
     };
+    // Methods
+    this.initialisePromo = function () {
+        // Get all items in promo bar
+        let promoItems = $("#promo > div");
+        // Set Values
+        this.promoBar.promoItems = promoItems;
+        this.promoBar.numberOfItems = promoItems.length;
+        // Initiate promo loop to show next item
+        this.startDelay();
+    }
+    this.startDelay = function () {
+        // Wait 4 seconds then show the next message
+        setTimeout(function () {
+            self.showNextPromoItem()
+        }, 4000);
+    }
 }
 $(document).ready(function () {
     // Instantiate new Common Class
